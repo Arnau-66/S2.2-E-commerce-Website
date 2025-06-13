2406e9aef14d0e36c5b3bbcf6973d3e51d6a5b95
Arnau Perez <arnauperezesteve@gmail.com>
2025-06-13 15:02:47 +0200

    Branch creation + WIP Exercice 3

--- js/shop.js
+++ js/shop.js
@@ -123,7 +123,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // Exercise 3
 const calculateTotal = () =>  {
-    // Calculate total price of the cart using the "cartList" array
+
+    let total = 0;
+
+    for (let i = 0; i < cart.length; i++) {
+        total += cart[i].price * cart[i].quantity;
+    }
+
+    console.log('Total del carrito:', total);
+    return total;
 }
 
 // Exercise 4
