fcf54cb04177b3a3f25d3da27f6dec85e60c7834
Arnau Perez <arnauperezesteve@gmail.com>
2025-06-15 18:18:05 +0200

    Adding key/value img on products objects

--- js/shop.js
+++ js/shop.js
@@ -6,6 +6,7 @@ const products = [
         name: 'cooking oil',
         price: 10.5,
         type: 'grocery',
+        img: 'images/Cooking oil',
         offer: {
             number: 3,
             percent: 20
@@ -15,13 +16,15 @@ const products = [
         id: 2,
         name: 'Pasta',
         price: 6.25,
-        type: 'grocery'
+        type: 'grocery',
+        img: 'images/Pasta',
     },
     {
         id: 3,
         name: 'Instant cupcake mixture',
         price: 5,
         type: 'grocery',
+        img: 'images/Instant cupcake mixture',
         offer: {
             number: 10,
             percent: 30
@@ -31,37 +34,43 @@ const products = [
         id: 4,
         name: 'All-in-one',
         price: 260,
-        type: 'beauty'
+        type: 'beauty',
+        img: 'images/All-in-one',
     },
     {
         id: 5,
         name: 'Zero Make-up Kit',
         price: 20.5,
-        type: 'beauty'
+        type: 'beauty',
+        img: 'images/Zero Make-up Kit',
     },
     {
         id: 6,
         name: 'Lip Tints',
         price: 12.75,
-        type: 'beauty'
+        type: 'beauty',
+        img: 'Lip Tints',
     },
     {
         id: 7,
         name: 'Lawn Dress',
         price: 15,
-        type: 'clothes'
+        type: 'clothes',
+        img: 'Lawn Dress',
     },
     {
         id: 8,
         name: 'Lawn-Chiffon Combo',
         price: 19.99,
-        type: 'clothes'
+        type: 'clothes',
+        img: 'images/Lawn-Chiffon Combo',
     },
     {
         id: 9,
         name: 'Toddler Frock',
         price: 9.99,
-        type: 'clothes'
+        type: 'clothes',
+        img: 'images/Toddler Frock',
     }
 ]
 
