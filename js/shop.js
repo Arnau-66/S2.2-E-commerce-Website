6b27f637bcd28b2c7cbcc6335eb50e201f0c177f
Arnau Perez <arnauperezesteve@gmail.com>
2025-06-15 18:19:47 +0200

    Adding .png on value img

--- js/shop.js
+++ js/shop.js
@@ -6,7 +6,7 @@ const products = [
         name: 'cooking oil',
         price: 10.5,
         type: 'grocery',
-        img: 'images/Cooking oil',
+        img: 'images/Cooking oil.png',
         offer: {
             number: 3,
             percent: 20
@@ -17,14 +17,14 @@ const products = [
         name: 'Pasta',
         price: 6.25,
         type: 'grocery',
-        img: 'images/Pasta',
+        img: 'images/Pasta.png',
     },
     {
         id: 3,
         name: 'Instant cupcake mixture',
         price: 5,
         type: 'grocery',
-        img: 'images/Instant cupcake mixture',
+        img: 'images/Instant cupcake mixture.png',
         offer: {
             number: 10,
             percent: 30
@@ -35,42 +35,42 @@ const products = [
         name: 'All-in-one',
         price: 260,
         type: 'beauty',
-        img: 'images/All-in-one',
+        img: 'images/All-in-one.png',
     },
     {
         id: 5,
         name: 'Zero Make-up Kit',
         price: 20.5,
         type: 'beauty',
-        img: 'images/Zero Make-up Kit',
+        img: 'images/Zero Make-up Kit.png',
     },
     {
         id: 6,
         name: 'Lip Tints',
         price: 12.75,
         type: 'beauty',
-        img: 'Lip Tints',
+        img: 'images/Lip Tints.png',
     },
     {
         id: 7,
         name: 'Lawn Dress',
         price: 15,
         type: 'clothes',
-        img: 'Lawn Dress',
+        img: 'images/Lawn Dress.png',
     },
     {
         id: 8,
         name: 'Lawn-Chiffon Combo',
         price: 19.99,
         type: 'clothes',
-        img: 'images/Lawn-Chiffon Combo',
+        img: 'images/Lawn-Chiffon Combo.png',
     },
     {
         id: 9,
         name: 'Toddler Frock',
         price: 9.99,
         type: 'clothes',
-        img: 'images/Toddler Frock',
+        img: 'images/Toddler Frock.png',
     }
 ]
 
