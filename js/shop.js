c5121623cb663c79a7d41a8e9302bb2d22264cb8
Arnau Perez <arnauperezesteve@gmail.com>
2025-06-17 10:49:41 +0200

    Deleting console logs and comments on shop.js

--- js/shop.js
+++ js/shop.js
@@ -1,4 +1,4 @@
-// If you have time, you can move this variable "products" to a json or js file and load the data in this js. It will look more professional
+
 const products = [
     
     {
@@ -74,17 +74,12 @@ const products = [
     }
 ]
 
-// => Reminder, it's extremely important that you debug your code. 
-// ** It will save you a lot of time and frustration!
-// ** You'll understand the code better than with console.log(), and you'll also find errors faster. 
-// ** Don't hesitate to seek help from your peers or your mentor if you still struggle with debugging.
 
-// Improved version of cartList. Cart is an array of products (objects), but each one has a quantity field to define its quantity, so these products are not repeated.
 const cart = [];
 
 const total = 0;
 
-// Exercise 1
+
 const buy = (id) => {
 
     const product = products.find(item => item.id === id);
@@ -96,21 +91,18 @@ const buy = (id) => {
         const productToAdd = { ...product, quantity: 1};
         cart.push(productToAdd);
     }
-    console.log(cart);
     updateCartCount();
 }
 
 
-// Exercise 2
+
 const cleanCart = () => {
     cart.length = 0;
     printCart();
-    console.log('Carrito vaciado:', cart);
     updateCartCount();
 };
 
 
-// Exercise 3
 const calculateTotal = () =>  {
 
     let total = 0;
@@ -118,11 +110,9 @@ const calculateTotal = () =>  {
     for (let i = 0; i < cart.length; i++) {
         total += cart[i].price * cart[i].quantity;
     }
-    console.log('Total del carrito:', total);
     return total;
 }
 
-// Exercise 4
 const applyPromotionsCart = () =>  {
     cart.forEach(product => { 
       delete product.subtotalWithDiscount;
@@ -134,7 +124,6 @@ const applyPromotionsCart = () =>  {
       });
 }
 
-// Exercise 5
 
 const printCart = () => {
   const cartList = document.getElementById('cart_list');
@@ -170,11 +159,6 @@ const printCart = () => {
 };
 
 
-
-
-// ** Nivell II **
-
-// Exercise 7
 const removeFromCart = (id) => {
   const productIndex = cart.findIndex(item => item.id === id);
 
